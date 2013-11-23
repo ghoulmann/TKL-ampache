5586a2f816db097154da7be90086d9b0abde2512
root <root@tkldev.(none)>
2013-11-23 22:37:04 +0000

    it works

--- overlay/usr/lib/inithooks/bin/ampache_pass.py
+++ overlay/usr/lib/inithooks/bin/ampache_pass.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
-"""Set Ampache admin password
+"""Set ampache admin password
 
 Option:
 --pass= unless provided, will ask interactively
---email= unless provided, will ask interactively
+
 """
 
 import sys
 import getopt
 
-import subprocess
+import crypt
+import random
+import hashlib
 
 from dialog_wrapper import Dialog
+from mysqlconf import MySQL
 
 def usage(s=None):
     if s:
@@ -22,37 +25,28 @@ def usage(s=None):
 
 def main():
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], "h", ['help', 'pass=', 'email='])
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h", ['help', 'pass='])
     except getopt.GetoptError, e:
         usage(e)
 
-    email = ""
     password = ""
-    adduser="/var/www/ampache/bin/install/add_user.inc"
-
     for opt, val in opts:
         if opt in ('-h', '--help'):
             usage()
         elif opt == '--pass':
             password = val
-        elif opt == '--email':
-            email = val
 
- 
     if not password:
         d = Dialog('TurnKey Linux - First boot configuration')
         password = d.get_password(
             "Ampache Password",
             "Enter new password for the Ampache 'admin' account.")
 
-    if not email:
-        d = Dialog('TurnKey Linux - First boot configuration')
-        email = d.get_email(
-            "Admin Email",
-            "Enter an email address for the Ampache 'admin' account.",
-            "admin@example.com")
+    hash = hashlib.sha256(password).hexdigest()
 
-    subprocess.call(["php", "/var/www/ampache/bin/install/add_user.inc", "-ladmin", "-uadmin", "-p" + password, "-e" +  email])
+    m = MySQL()
+    m.execute('UPDATE ampache.user SET password=\"%s\" WHERE username=\"admin\";' % hash)
+
 
 if __name__ == "__main__":
     main()
